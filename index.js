eb999c3040cf759765ab4568ff87c4720f897088
yugmani <yugmani@yahoo.com>
2022-03-11 22:44:27 -0800

    comparing ?? or ||

--- index.js
+++ index.js
@@ -38,3 +38,26 @@ let nickName = "Supercoder";
 
 //shows the first defined value;
 console.log(firstName ?? lastName ?? nickName ?? "Supercoder");   // Supercoder
+
+
+// Comparison with ||
+// The OR || operator can be used in the same way as ??
+
+// For example, in the code above we could replace ?? with || and still get the same result:
+console.log(firstName || lastName || nickName || "Anonymous");  // Supercoder
+
+// The important difference between them is that:
+
+//1. || returns the first truthy value.
+//2. ?? returns the first defined value.
+
+// In other words, || doesn’t distinguish between false, 0, an empty string "" and null/undefined. 
+// They are all the same – falsy values. If any of these is the first argument of ||, then we’ll get the second argument as the result.
+// In practice though, we may want to use default value only when the variable is null/undefined. That is, when the value is really unknown/not set.
+
+let height = 0;
+console.log(height || 100); //100;
+console.log(height ?? 100); // 0
+
+// In practice, the zero height is often a valid value, that shouldn’t be replaced with the default. So ?? does just the right thing.
+
